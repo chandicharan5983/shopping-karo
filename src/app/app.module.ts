6fd9ff16d4252b04883a28928aaf4a494618537d
SHYAMSTEEL\samiran.dhali <samiran.dhali@shyamsteel.com>
2024-01-24 22:18:25 +0530

    add material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialAngularModule } from './material-import.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-import.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+@NgModule({
+    imports: [
+    ],
+    exports: [
+        MatSlideToggleModule
+    ],
+    providers: [],
+})
+export class MaterialAngularModule { }
