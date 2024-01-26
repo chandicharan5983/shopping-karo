8062e9e11cf43e5618374babf2375419237c8823
SHYAMSTEEL\samiran.dhali <samiran.dhali@shyamsteel.com>
2024-01-27 00:17:44 +0530

    Search bar design

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -5,6 +5,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { MaterialAngularModule } from '../material-import.module';
 
 
 @NgModule({
@@ -16,8 +17,9 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     CommonModule,
+    MaterialAngularModule
   ],
-  exports : [
+  exports: [
     HeaderComponent
   ]
 })
