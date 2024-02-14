4468b24f307f01fc12c5aa8781a76d7c78426e7f
SHYAMSTEEL\samiran.dhali <samiran.dhali@shyamsteel.com>
2024-02-14 21:34:30 +0530

    Design Changes

--- src/app/body/body-routing.module.ts
+++ src/app/body/body-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FashionComponent } from './fashion/fashion.component';
 import { HomeComponent } from './home/home.component';
+import { WorkRoomComponent } from './work-room/work-room.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'fashion', component: FashionComponent },
+    { path: 'workroom', component: WorkRoomComponent },
 ];
 
 @NgModule({
--- src/app/body/work-room/work-room.component.ts
+++ src/app/body/work-room/work-room.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class WorkRoomComponent {
 
+  policyArray = [
+    {
+      name: 'Bonus Plus',
+      dsc: 'Make fun of shopping and collect bonuses',
+      img: 'card_giftcard',
+    },
+    {
+      name: 'Free Shipping',
+      dsc: 'Free shipping on all orders over $99',
+      img: 'local_shipping',
+    },
+    {
+      name: 'Money Back Guarantee',
+      dsc: '30 Days money return guarantee',
+      img: 'monetization_on',
+    },
+    {
+      name: 'Online Support 24/7',
+      dsc: 'Call us: (+100) 123 456 7890',
+      img: 'history',
+    },
+  ];
+
 }
